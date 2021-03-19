545095273b9133eeda5ec1ff3584411f9eb4ac53
Alex Nagel <mail@alex-nagel.de>
2021-03-19 22:03:14 +0100

    Initial commit

---
+++ Red Metronome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Red Metronome
+//
+//  Created by Alexander Nagel on 19.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
